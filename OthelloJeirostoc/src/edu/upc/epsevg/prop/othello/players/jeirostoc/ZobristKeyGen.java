b8b72db97f75f8a7ab857a41f82adfd8a3e4460e
RabadanDotDev <77171479+RabadanDotDev@users.noreply.github.com>
2022-12-06 16:46:03 +0100

    add zobrist key gen

---
+++ OthelloJeirostoc/src/edu/upc/epsevg/prop/othello/players/jeirostoc/ZobristKeyGen.java
@@ -0,0 +1,145 @@
+package edu.upc.epsevg.prop.othello.players.jeirostoc;
+
+import edu.upc.epsevg.prop.othello.CellType;
+import java.awt.Point;
+import java.util.BitSet;
+import java.util.Random;
+
+/**
+ * Helper class to generate zobrist keys for the othello game
+ * 
+ * @author raul
+ * @author josep
+ */
+public class ZobristKeyGen {    
+    public enum BoardVariation{
+        BASE(0),
+        ROT90(1),
+        ROT180(2),
+        ROT270(3),
+        FLIP(4),
+        FLIPROT90(5),
+        FLIPROT180(6),
+        FLIPROT270(7),
+        COUNT(8);
+        
+        public final int v;
+        
+        private BoardVariation(int v) {
+            this.v = v;
+        }
+        
+        public BoardVariation valueOf(int v) {
+            return switch (v) {
+                case 0  -> BASE;
+                case 1  -> ROT90;
+                case 2  -> ROT180;
+                case 3  -> ROT270;
+                case 4  -> FLIP;
+                case 5  -> FLIPROT90;
+                case 6  -> FLIPROT180;
+                case 7  -> FLIPROT270;
+                default -> BASE;
+            };
+        }
+    }
+    
+    public static BitSet getZobristValue(int x, int y, CellType c, BoardVariation bp) {
+        return VALUES[posToIndex(x, y, cellTypeToStateNum(c) ,bp)];
+    }
+    
+    public static BitSet getZobristValue(Point p, CellType c, BoardVariation bp) {
+        return VALUES[posToIndex(p.x, p.y, cellTypeToStateNum(c) ,bp)];
+    }
+    
+    private static final BitSet[] VALUES;
+    private final static int BOARD_SIZE = 8;
+    private final static int BOARD_STATES = 3;
+    private final static int MAP_KEYS_COUNT = BOARD_SIZE*BOARD_SIZE*BOARD_STATES;
+    
+    static {
+        // Init
+        Random r = new Random();
+        long[] l = new long[]{0};
+        VALUES = new BitSet[BoardVariation.COUNT.v*MAP_KEYS_COUNT + 1];
+        
+        // Generate a zobrist value for each position and state of the board
+        for (int y = 0; y < BOARD_SIZE; y++) {
+            for (int x = 0; x < BOARD_SIZE; x++) {
+                for (int s = 0; s < BOARD_STATES; s++) {
+                    // Generate zobrist value for this position and state
+                    l[0] = r.nextLong();
+                    BitSet zv = BitSet.valueOf(l);
+                    
+                    // Update all variations
+                    VALUES[inversePosToIndex(x, y, s, BoardVariation.BASE)]       = zv;
+                    VALUES[inversePosToIndex(x, y, s, BoardVariation.ROT90)]      = zv;
+                    VALUES[inversePosToIndex(x, y, s, BoardVariation.ROT180)]     = zv;
+                    VALUES[inversePosToIndex(x, y, s, BoardVariation.ROT270)]     = zv;
+                    VALUES[inversePosToIndex(x, y, s, BoardVariation.FLIP)]       = zv;
+                    VALUES[inversePosToIndex(x, y, s, BoardVariation.FLIPROT90)]  = zv;
+                    VALUES[inversePosToIndex(x, y, s, BoardVariation.FLIPROT180)] = zv;
+                    VALUES[inversePosToIndex(x, y, s, BoardVariation.FLIPROT270)] = zv;
+                }
+            }
+        }
+        
+        // Generate the value to indicate that it is the turn of player 1
+        l[0] = r.nextLong();
+        BitSet zv = BitSet.valueOf(l);
+        VALUES[BoardVariation.COUNT.v*MAP_KEYS_COUNT] = zv;
+    }
+    
+    private static int cellTypeToStateNum(CellType c) {
+        return switch (c) {
+            case EMPTY   -> 0;
+            case PLAYER1 -> 1;
+            case PLAYER2 -> 2;
+            default      -> 0;
+        };
+    }
+    
+    /**
+     * Get the index of the Zobrist value in VALUES corresponding to the board 
+     * position (x, y) with cell state s after doing the transformation specified
+     * by the variation bp
+     * 
+     * @param x The x coordinate.
+     * @param y The y coordinate.
+     * @param s The cell state.
+     * @param bp The board variation
+     * @return The index
+     */
+    private static int posToIndex(int x, int y, int s, BoardVariation bp) {
+        return MAP_KEYS_COUNT*bp.v + (y*BOARD_SIZE + x)*BOARD_STATES + s;
+    }
+    
+    /**
+     * Get the index of the Zobrist value in VALUES corresponding to the board 
+     * position (x, y) with cell state s after doing the inverse transformation 
+     * specified by the variation bp. The inverse transformation is understood 
+     * to be the cell which x, y would fall in after doing bp
+     * 
+     * @param x The x coordinate.
+     * @param y The y coordinate.
+     * @param s The cell state.
+     * @param bp The board variation
+     * @return The index
+     */
+    private static int inversePosToIndex(int x, int y, int s, BoardVariation bp) {
+        int invX, invY;
+        switch (bp) {
+            case BASE       -> {invX = x;              invY = y;              }
+            case ROT90      -> {invX = y;              invY = BOARD_SIZE-x-1; }
+            case ROT180     -> {invX = BOARD_SIZE-x-1; invY = BOARD_SIZE-y-1; }
+            case ROT270     -> {invX = BOARD_SIZE-y-1; invY = x;              }
+            case FLIP       -> {invX = x;              invY = BOARD_SIZE-y-1; }
+            case FLIPROT90  -> {invX = y;              invY = x;              }
+            case FLIPROT180 -> {invX = BOARD_SIZE-x-1; invY = y;              }
+            case FLIPROT270 -> {invX = BOARD_SIZE-y-1; invY = BOARD_SIZE-x-1; }
+            default         -> {invX = x;              invY = y;              }
+        }
+        
+        return MAP_KEYS_COUNT*bp.v + (invY*BOARD_SIZE + invX)*BOARD_STATES + s;
+    }
+}
---
+++ OthelloJeirostoc/test/edu/upc/epsevg/prop/othello/players/jeirostoc/ZobristKeyGenTest.java
@@ -0,0 +1,69 @@
+package edu.upc.epsevg.prop.othello.players.jeirostoc;
+
+import edu.upc.epsevg.prop.othello.CellType;
+import java.awt.Point;
+import java.util.BitSet;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runners.Parameterized;
+
+/**
+ * @author raul
+ * @author raul
+ */
+public class ZobristKeyGenTest {
+    Point pBase       = new Point(1, 0);
+    Point pRot90      = new Point(7, 1);
+    Point pRot180     = new Point(6, 7);
+    Point pRot270     = new Point(0, 6);
+    Point pFlip       = new Point(1, 7);
+    Point pFlipRot90  = new Point(0, 1);
+    Point pFlipRot180 = new Point(6, 0);
+    Point pFlipRot270 = new Point(7, 6);
+
+    private Point getPos(ZobristKeyGen.BoardVariation bv) {
+        return switch (bv) {
+            case BASE       -> pBase;
+            case ROT90      -> pRot90;
+            case ROT180     -> pRot180;
+            case ROT270     -> pRot270;
+            case FLIP       -> pFlip;
+            case FLIPROT90  -> pFlipRot90;
+            case FLIPROT180 -> pFlipRot180;
+            case FLIPROT270 -> pFlipRot270;
+            default         -> pBase;
+        };
+    }
+    
+    private void assertEqualZobristKeyWith(CellType cellType, ZobristKeyGen.BoardVariation bv) {
+        BitSet k1,k2;
+        
+        // Test 
+        k1 = ZobristKeyGen.getZobristValue(
+                pBase, cellType, bv
+        );
+        k2 = ZobristKeyGen.getZobristValue(
+                getPos(bv), cellType, ZobristKeyGen.BoardVariation.BASE
+        );
+        
+        assertEquals(k1, k2);
+    }
+    
+    private void assertEqualZobristKeyWith(ZobristKeyGen.BoardVariation bv) {
+        assertEqualZobristKeyWith(CellType.EMPTY,  bv);
+        assertEqualZobristKeyWith(CellType.PLAYER1, bv);
+        assertEqualZobristKeyWith(CellType.PLAYER2, bv);
+    }
+    
+    @Test
+    public void testGetZobristValue() {
+        assertEqualZobristKeyWith(ZobristKeyGen.BoardVariation.BASE);
+        assertEqualZobristKeyWith(ZobristKeyGen.BoardVariation.ROT90);
+        assertEqualZobristKeyWith(ZobristKeyGen.BoardVariation.ROT180);
+        assertEqualZobristKeyWith(ZobristKeyGen.BoardVariation.ROT270);
+        assertEqualZobristKeyWith(ZobristKeyGen.BoardVariation.FLIP);
+        assertEqualZobristKeyWith(ZobristKeyGen.BoardVariation.FLIPROT90);
+        assertEqualZobristKeyWith(ZobristKeyGen.BoardVariation.FLIPROT180);
+        assertEqualZobristKeyWith(ZobristKeyGen.BoardVariation.FLIPROT270);
+    }
+}
