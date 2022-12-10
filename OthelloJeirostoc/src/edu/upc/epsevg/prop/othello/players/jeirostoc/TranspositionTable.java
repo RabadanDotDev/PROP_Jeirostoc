2365a789f2a2f2c6b7ba734094938f16e7f826fe
RabadanDotDev <77171479+RabadanDotDev@users.noreply.github.com>
2022-12-11 00:40:49 +0100

    rework transposition table

--- OthelloJeirostoc/src/edu/upc/epsevg/prop/othello/players/jeirostoc/TranspositionTable.java
+++ OthelloJeirostoc/src/edu/upc/epsevg/prop/othello/players/jeirostoc/TranspositionTable.java
@@ -1,10 +1,5 @@
 package edu.upc.epsevg.prop.othello.players.jeirostoc;
 
-import edu.upc.epsevg.prop.othello.CellType;
-import java.awt.Point;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
 
 /**
  * Transposition table of HeuristicStatus capable of giving a list of explorable
@@ -13,157 +8,207 @@ import java.util.HashMap;
  * @author raul
  */
 class TranspositionTable {
-//    static class TTKey {
-//        int hash;
-//
-//        public TTKey(HeuristicStatus hs) {
-//            this.hash = hs.hashCode();
-//        }
-//        
-//        public void update(HeuristicStatus hs) {
-//            this.hash = hs.hashCode();
-//        }
-//        
-//        @Override
-//        public int hashCode() {
-//            return hash;
-//        }
-//
-//        @Override
-//        public boolean equals(Object obj) {
-//            if (this == obj) {
-//                return true;
-//            }
-//            if (obj == null) {
-//                return false;
-//            }
-//            if (getClass() != obj.getClass()) {
-//                return false;
-//            }
-//            final TTKey other = (TTKey) obj;
-//            return this.hash == other.hash;
-//        }
-//    }
-//    
-//    class TTValue {
-//        double selectedHeuristic;
-//        int distanceToBottom;
-//
-//        public TTValue(double selectedHeuristic, int distanceToBottom) {
-//            this.selectedHeuristic = selectedHeuristic;
-//            this.distanceToBottom = distanceToBottom;
-//        }
-//    }
-//    
-//    private class SortMin implements Comparator<HeuristicStatus> {
-//        @Override
-//        public int compare(HeuristicStatus t1, HeuristicStatus t2) {
-//            return Double.compare(
-//                    getLastRegisteredHeuristic(t1), 
-//                    getLastRegisteredHeuristic(t2)
-//            );
-//        }
-//    }
-//    
-//    private class SortMax implements Comparator<HeuristicStatus> {
-//        @Override
-//        public int compare(HeuristicStatus t1, HeuristicStatus t2) {
-//            return Double.compare(
-//                    getLastRegisteredHeuristic(t2), 
-//                    getLastRegisteredHeuristic(t1)
-//            );
-//        }
-//    }
-//    
-//    private final HashMap<TTKey, TTValue>[] _table;
-//    private final SortMin _sMin = new SortMin();
-//    private final SortMax _sMax = new SortMax();
-//    
-//    public TranspositionTable() {
-//        this._table = new HashMap[8*8-4+1];
-//        for (int i = 0; i < _table.length; i++) {
-//            _table[i] = new HashMap<>();
-//        }
-//    }
-//    
-//    public void clear() {
-//    }
-//    
-//    /**
-//     * Get a list of nodes that can be explored from HeuristicStatus with the 
-//     * internal information from the transposition table.
-//     * 
-//     * @param hs The HeuristicStatus
-//     * @return The list of nodes
-//     */
-//    public ArrayList<HeuristicStatus> getNextExplorableNodes(HeuristicStatus hs, boolean isMax) {
-//        ArrayList<HeuristicStatus> hsl = new ArrayList<>();
-//        for (Point p : hs.getMoves()) {
-//            HeuristicStatus next = hs.getNextStatus(p);
-//            hsl.add(next);
-//        }
-//        hsl.sort(isMax ? _sMax : _sMin);
-//        return hsl;
-//    }
-//
-//    /**
-//     * Check if the heuristicStatus has been already seen with at least the 
-//     * given distance to bottom
-//     * 
-//     * @param hs The HeuristicStatus
-//     * @param minDistanceToBottom The minimum distance to bottom
-//     * @return True if it has been registered the exploration of this node, 
-//     * false otherwise
-//     */
-//    public boolean seen(HeuristicStatus hs, int minDistanceToBottom) {
-//        return get(hs, minDistanceToBottom) != null;
-//    }
-//    
-//    /**
-//     * Get the last registered value if hs has already been seen with at least
-//     * the given distance to bottom
-//     * 
-//     * @param hs The HeuristicStatus
-//     * @param minDistanceToBottom The minimum distance to bottom
-//     * @return The entry if it exists, null if it does not
-//     */
-//    public TTValue get(HeuristicStatus hs, int minDistanceToBottom) {
-//        TTValue v = _table[hs.getMovementCount()].get(hs.getTTKey());
-//        if(v != null && minDistanceToBottom <= v.distanceToBottom ) 
-//            return v;
-//        else
-//            return null;
-//    }
-//    
-//    /**
-//     * Get the last registered heuristic for hs or 0 in case it does not exist.
-//     * 
-//     * @param hs The HeuristicStatus
-//     * @return The heuristic or 0 if hs was not registered
-//     */
-//    private double getLastRegisteredHeuristic(HeuristicStatus hs) {
-//        TTValue v = _table[hs.getMovementCount()].get(hs.getTTKey());
-//        if(v == null) 
-//            return 0;
-//        else                                           
-//            return v.selectedHeuristic;
-//    }
-//
-//    /**
-//     * Register exploration of node hs if distance to bottom is greater or equal 
-//     * to what we had before
-//     * 
-//     * @param hs The HeuristicStatus
-//     * @param selectedHeuristic The selected heuristic
-//     */
-//    public void register(HeuristicStatus hs, double selectedHeuristic, int distanceToBottom) {
-//        TTValue v = _table[hs.getMovementCount()].get(hs.getTTKey());
-//        if(v == null) {
-//            v = new TTValue(selectedHeuristic, distanceToBottom);
-//            _table[hs.getMovementCount()].put(hs.getTTKey(), v);
-//        } else if(v.distanceToBottom <= distanceToBottom) {
-//            v.selectedHeuristic = selectedHeuristic;
-//            v.distanceToBottom  = distanceToBottom;
-//        }
-//    }
+    /**
+     * Mask to extract the "selected heuristic" from the 8 lower bytes of the
+     * entry.
+     */
+    private static final long SELECTED_HEURISTIC_MASK  = 0xffffffffL;
+    
+    /**
+     * Mask to extract the low byte.
+     */
+    private static final long BYTE_MASK = 0xffL;
+    
+    /**
+     * Amount of bits to shift to the right to put the byte specifying the 
+     * selected movement from the entry in the low position.
+     */
+    private static final int  SELECTED_MOVEMENT_SHIFT = 32;
+    
+    /**
+     * Amount of bits to shift to the right to put the byte specifying the 
+     * depth below from the entry in the low position.
+     */
+    private static final int  DEPTH_BELOW_SHIFT = 40;
+    
+    /**
+     * Mask with a 1 on the position of the bit isExact from the flags field of 
+     * the entry.
+     */
+    private static final long FLAG_IS_EXACT_MASK = 1L << (48);
+    
+    /**
+     * Mask with a 1 on the position of the bit isAlpha from the flags field of 
+     * the entry.
+     */
+    private static final long FLAG_IS_ALPHA_MASK = 1L << (49);
+    
+    /**
+     * Mask with a 1 on the position of the bit isValidEntry from the flags 
+     * field of the entry.
+     */
+    private static final long FLAG_IS_VALID_ENTRY_MASK = 1L << (50);
+    
+    /**
+     * Default number of entries in the transposition table.
+     */
+    private static final long DEF_NUM_ENTRIES = 134204621;
+    
+    /**
+     * Number of entries in the transposition table.
+     */
+    private final long _numEntries;
+    
+    /**
+     * Transposition table internal data.
+     */
+    private final long[] _table;
+
+    /**
+     * Default constructor.
+     */
+    public TranspositionTable() {
+        _numEntries = DEF_NUM_ENTRIES;
+        _table = new long[(int)_numEntries*2];
+    }
+    
+    /**
+     * Constructor with a custom size table size.
+     * 
+     * @param numEntries The number of entries in the table
+     */
+    public TranspositionTable(int numEntries) {
+        _numEntries = numEntries;
+        _table = new long[(int)_numEntries*2];
+    }
+    
+    /**
+     * Register an entry to the TranspositionTable. It will be added if there is
+     * no collision, if the collision has a different min Zobrist hash or the 
+     * depthBelow is lower or equal than the provided
+     * 
+     * @param s The status to register
+     * @param selectedHeuristic The selected heuristic for this status
+     * @param selectedMovementBitIndex The selected movement expressed in x*SIZE
+     * + y format
+     * @param depthBelow The depth explored below s
+     * @param isExact The flag to indicate if the heuristic is exact (true) or 
+     * was calculated with pruning (false)
+     * @param isAlpha The flag to indicate if the heuristic is an upper bound 
+     * (true) or a lower bound (false).
+     */
+    public void register(Status s, float selectedHeuristic, byte selectedMovementBitIndex, byte depthBelow, boolean isExact, boolean isAlpha) {
+        long key = s.getMinZobristKey();
+        int index = (int) (key%_numEntries)*2;
+        
+        long currentKey   = _table[index];
+        long currentEntry = _table[index+1];
+        
+        if (!extractIsValidEntry(currentEntry) || currentKey != key || extractDepthBelow(currentEntry) <= depthBelow) {
+            _table[index  ] = key;
+            _table[index+1] = toEntry(selectedHeuristic, selectedMovementBitIndex, depthBelow, isExact, isAlpha);
+        }
+    }
+    
+    /**
+     * Read the entry in the bitpacked format from the transposition table. If a
+     * registered entry can't be found, it returns 0.
+     * 
+     * @param s The status to register
+     * @return The entry
+     */
+    public long readEntry(Status s) {
+        long key = s.getMinZobristKey();
+        int index = (int) (key%_numEntries)*2;
+        
+        long currentKey   = _table[index];
+        long currentEntry = _table[index+1];
+        
+        if(extractIsValidEntry(currentEntry) && currentKey == key) 
+            return currentEntry;
+        else
+            return 0;
+    }
+    
+    /**
+     * Express the component entries in the bitpacked format.
+     * 
+     * @param selectedHeuristic The selected heuristic
+     * @param selectedMovementBitIndex The selected movement expressed in x*SIZE
+     * + y format
+     * @param depthBelow The depth explored
+     * @param isExact The flag to indicate if the heuristic is exact (true) or 
+     * was calculated with pruning (false)
+     * @param isAlpha The flag to indicate if the heuristic is an upper bound 
+     * (true) or a lower bound (false).
+     * @return 
+     */
+    public static long toEntry(float selectedHeuristic, byte selectedMovementBitIndex, byte depthBelow, boolean isExact, boolean isAlpha) {
+        return ((long)Float.floatToRawIntBits(selectedHeuristic))          |
+               ((long)selectedMovementBitIndex) << SELECTED_MOVEMENT_SHIFT |
+               ((long)depthBelow)               <<       DEPTH_BELOW_SHIFT |
+               (isExact ? FLAG_IS_EXACT_MASK  : 0L)                        |
+               (isAlpha ? FLAG_IS_ALPHA_MASK  : 0L)                        |
+               (FLAG_IS_VALID_ENTRY_MASK);
+    }
+    
+    /**
+     * Extract the selected heuristic from the entry.
+     * 
+     * @param entry The entry to extract from
+     * @return The selected heuristic
+     */
+    public static float extractSelectedHeuristic(long entry) {
+        return Float.intBitsToFloat((int)(entry & SELECTED_HEURISTIC_MASK));
+    }
+    
+    /**
+     * Extract the selected movement from the entry.
+     * 
+     * @param entry The entry to extract from
+     * @return The selected movement
+     */
+    public static byte extractSelectedMovement(long entry) {
+        return (byte)((entry >> SELECTED_MOVEMENT_SHIFT) & BYTE_MASK);
+    }
+    
+    /**
+     * Extract the depthBelow from the entry.
+     * 
+     * @param entry The entry to extract from
+     * @return The depthBelow
+     */
+    public static byte extractDepthBelow(long entry) {
+        return (byte)((entry >> DEPTH_BELOW_SHIFT) & BYTE_MASK);
+    }
+    
+    /**
+     * Extract the isExact flag from the entry.
+     * 
+     * @param entry The entry to extract from
+     * @return The isExact flag
+     */
+    public static boolean extractIsExact(long entry) {
+        return (entry & FLAG_IS_EXACT_MASK) != 0;
+    }
+    
+    /**
+     * Extract the isAlpha flag from the entry.
+     * 
+     * @param entry The entry to extract from
+     * @return The isAlpha flag
+     */
+    public static boolean extractIsAlpha(long entry) {
+        return (entry & FLAG_IS_ALPHA_MASK) != 0;
+    }
+    
+    /**
+     * Extract the isValid flag from the entry.
+     * 
+     * @param entry The entry to extract from
+     * @return The isValid flag
+     */
+    public static boolean extractIsValidEntry(long entry) {
+        return (entry & FLAG_IS_VALID_ENTRY_MASK) != 0;
+    }
 }
---
+++ OthelloJeirostoc/test/edu/upc/epsevg/prop/othello/players/jeirostoc/TranspositionTableTest.java
@@ -0,0 +1,91 @@
+package edu.upc.epsevg.prop.othello.players.jeirostoc;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author raul
+ */
+public class TranspositionTableTest {    
+    @Test
+    public void testSupositionsBitPacking() {
+        float heuristic        = 7;
+        byte  bitIndexMovement = 0b1010010;
+        byte  depthBelow       = 0b1111111;
+        byte  flags            = 0b1001001;
+        
+        long field1 = Float.floatToRawIntBits(heuristic);
+        long field2 = bitIndexMovement;
+        long field3 = depthBelow;
+        long field4 = flags;
+        
+        long entry = 
+                field1       | // 32 bits
+                field2 << 32 | // 8  bits
+                field3 << 40 | // 8  bits
+                field4 << 48;  // 8  bits
+        
+        long extractedField1 = entry       & 0xffffffffL;
+        long extractedField2 = entry >> 32 & 0x000000ff;
+        long extractedField3 = entry >> 40 & 0x000000ff;
+        long extractedField4 = entry >> 48 & 0x000000ff;
+        
+        assertEquals(field1, extractedField1);
+        assertEquals(field2, extractedField2);
+        assertEquals(field3, extractedField3);
+        assertEquals(field4, extractedField4);
+        
+        float extractedHeuristic        = Float.intBitsToFloat((int)field1);
+        byte  extractedBitIndexMovement = (byte)field2;
+        byte  extractedDepthBelow       = (byte)field3;
+        byte  extractedFlags            = (byte)field4;
+        
+        
+        assertEquals(heuristic, extractedHeuristic, 0.0);
+        assertEquals(bitIndexMovement, extractedBitIndexMovement, 0.0);
+        assertEquals(depthBelow, extractedDepthBelow, 0.0);
+        assertEquals(flags, extractedFlags, 0.0);
+    }
+    
+    @Test
+    public void testToEntryAndExtract1() {
+        float selectedHeuristic = 1.051f;
+        byte selectedMovementBitIndex = 53;
+        byte depthBelow = 7;
+        boolean isExact = true;
+        boolean isAlpha = true;
+        
+        long entry = TranspositionTable.toEntry(selectedHeuristic, selectedMovementBitIndex, depthBelow, isExact, isAlpha);
+        
+        assertEquals(TranspositionTable.extractSelectedHeuristic(entry), selectedHeuristic, 0.0f);
+        assertEquals(TranspositionTable.extractSelectedMovement(entry),  selectedMovementBitIndex);
+        assertEquals(TranspositionTable.extractDepthBelow(entry),        depthBelow);
+        assertEquals(TranspositionTable.extractIsExact(entry),           isExact);
+        assertEquals(TranspositionTable.extractIsAlpha(entry),           isAlpha);
+        assertEquals(TranspositionTable.extractIsValidEntry(entry),      true);
+    }
+    
+    @Test
+    public void testToEntryAndExtract2() {
+        float selectedHeuristic = Float.POSITIVE_INFINITY;
+        byte selectedMovementBitIndex = 3;
+        byte depthBelow = 10;
+        boolean isExact = false;
+        boolean isAlpha = false;
+        
+        long entry = TranspositionTable.toEntry(selectedHeuristic, selectedMovementBitIndex, depthBelow, isExact, isAlpha);
+        
+        assertEquals(TranspositionTable.extractSelectedHeuristic(entry), selectedHeuristic, 0.0f);
+        assertEquals(TranspositionTable.extractSelectedMovement(entry),  selectedMovementBitIndex);
+        assertEquals(TranspositionTable.extractDepthBelow(entry),        depthBelow);
+        assertEquals(TranspositionTable.extractIsExact(entry),           isExact);
+        assertEquals(TranspositionTable.extractIsAlpha(entry),           isAlpha);
+        assertEquals(TranspositionTable.extractIsValidEntry(entry),      true);
+    }
+    
+    @Test
+    public void testEmptyEntry() {
+        
+    }
+}
